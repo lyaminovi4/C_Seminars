31814f503ebf50b3e057b454fa0b3ff8b5926390
Constantine <lyaminovi4@gmail.com>
2022-12-06 02:42:16 +0500

    task0 - первый семинар

---
+++ task0/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на себя)
+
+// 1й вариант
+// Console.Write ("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(number*number);
+
+// 2й вариант
+Console.Write ("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int res = number*number;
+Console.WriteLine("Квадрат числа: " + res);
+
+
