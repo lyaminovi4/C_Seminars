700a3842b6bd69e6f472a5bd5e913cb7ca87217b
Constantine <lyaminovi4@gmail.com>
2022-12-06 03:02:41 +0500

    task1 - первый семинар

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Console.Write ("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(number*number);
+
+Console.Write ("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()); 
+Console.Write ("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()); 
+
+if (b*b == a){
+    Console.WriteLine ("Ответ: Да");
+}
+else {
+    Console.WriteLine ("Ответ: Нет");
+}
